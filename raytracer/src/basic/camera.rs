040822b2b56506ca4533fb812bfd8028a8815625
root <root@Sirius.localdomain>
2023-07-14 09:17:16 +0800

    book1-12

--- raytracer/src/basic/camera.rs
+++ raytracer/src/basic/camera.rs
@@ -1,66 +1,58 @@
-use super::clamp;
+use super::vec3::{Vec3, vec3_sub, vec3_tri_add, vec3_mul};
 use super::ray::Ray;
-use super::vec3::{vec3_mul, vec3_sub, vec3_tri_add, Vec3};
+use super::clamp;
 
 #[derive(Copy, Clone)]
 pub struct Camera {
-    origin: Vec3,
-    lower_left_corner: Vec3,
-    horizontal: Vec3,
-    vertical: Vec3,
+	origin: Vec3,
+	lower_left_corner: Vec3,
+	horizontal: Vec3,
+	vertical: Vec3,
 }
 
 impl Camera {
-    pub fn new() -> Self {
-        let aspect_ratio = 16.0 / 9.0;
-        let viewpoint_height = 2.0;
-        let viewpoint_width = aspect_ratio * viewpoint_height;
-        let focal_length = 1.0;
-        let origin_ = Vec3::set(0.0, 0.0, 0.0);
-        let horizontal_ = Vec3::set(viewpoint_width, 0.0, 0.0);
-        let vertical_ = Vec3::set(0.0, viewpoint_height, 0.0);
-        Camera {
-            origin: origin_,
-            horizontal: horizontal_,
-            vertical: vertical_,
-            lower_left_corner: vec3_sub(
-                &origin_,
-                &vec3_tri_add(
-                    &vec3_mul(&0.5, &horizontal_),
-                    &vec3_mul(&0.5, &vertical_),
-                    &Vec3::set(0.0, 0.0, focal_length),
-                ),
-            ),
-        }
-    }
-    pub fn get_ray(&self, u: &f64, v: &f64) -> Ray {
-        Ray::set(
-            self.origin,
-            vec3_tri_add(
-                &self.lower_left_corner,
-                &vec3_mul(u, &self.horizontal),
-                &vec3_mul(v, &self.vertical),
-            ),
-        )
-    }
+	pub fn new() -> Self {
+		let aspect_ratio = 16.0 / 9.0;
+		let viewpoint_height = 2.0;
+		let viewpoint_width = aspect_ratio * viewpoint_height;
+		let focal_length = 1.0;
+		let origin_ = Vec3::set(0.0, 0.0, 0.0);
+		let horizontal_ = Vec3::set(viewpoint_width, 0.0, 0.0);
+		let vertical_ = Vec3::set(0.0, viewpoint_height, 0.0);
+		Camera {
+			origin: origin_,
+			horizontal: horizontal_,
+			vertical: vertical_,
+			lower_left_corner: vec3_sub(
+				&origin_, 
+				&vec3_tri_add(&vec3_mul(&0.5, &horizontal_), &vec3_mul(&0.5, &vertical_), &Vec3::set(0.0, 0.0, focal_length))
+			),
+		}
+	}
+	pub fn get_ray(&self, u: &f64, v: &f64) -> Ray {
+		Ray::set(
+			self.origin, 
+			vec3_tri_add(&self.lower_left_corner, &vec3_mul(u, &self.horizontal), &vec3_mul(v, &self.vertical)),
+		)
+	}
 }
 impl Default for Camera {
-    fn default() -> Self {
-        Self::new()
-    }
+	fn default() -> Self {
+		Self::new()
+	}
 }
 
 pub fn write_colour(pixel_colour: &Vec3, sample_per_pixel: &i32) -> Vec<u8> {
-    let scale: f64 = 1.0 / (*sample_per_pixel as f64);
-    let lr: f64 = (pixel_colour.x_dir * scale).sqrt();
-    let lg: f64 = (pixel_colour.y_dir * scale).sqrt();
-    let lb: f64 = (pixel_colour.z_dir * scale).sqrt();
-    // lr *= scale;
-    // lg *= scale;
-    // lb *= scale;
-    vec![
-        (clamp(&lr, &0.0, &0.999) * 256.0) as u8,
-        (clamp(&lg, &0.0, &0.999) * 256.0) as u8,
-        (clamp(&lb, &0.0, &0.999) * 256.0) as u8,
-    ]
+	let scale: f64 = 1.0 / (*sample_per_pixel as f64);
+	let lr: f64 = (pixel_colour.x_dir * scale).sqrt();
+	let lg: f64 = (pixel_colour.y_dir * scale).sqrt();
+	let lb: f64 = (pixel_colour.z_dir * scale).sqrt();
+	// lr *= scale;
+	// lg *= scale;
+	// lb *= scale;
+	vec![
+		(clamp(&lr, &0.0, &0.999) * 256.0) as u8, 
+		(clamp(&lg, &0.0, &0.999) * 256.0) as u8, 
+		(clamp(&lb, &0.0, &0.999) * 256.0) as u8
+	]
 }
--- raytracer/src/object/material.rs
+++ raytracer/src/object/material.rs
@@ -1,119 +1,100 @@
 use super::hittable::HitRecord;
 use crate::basic::ray::Ray;
-use crate::basic::vec3::{
-    generate_unit_vector, random_unit_vector, reflect, vec3_add, vec3_dot, Vec3,
-};
+use crate::basic::vec3::{Vec3, vec3_add, random_unit_vector, reflect, generate_unit_vector, vec3_dot, vec3_mul, random_in_unit_sphere};
 
 pub trait Material {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Vec3,
-        scattered: &mut Ray,
-    ) -> bool;
+	fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool;
 }
 
 #[derive(Copy, Clone)]
 pub struct Lambertian {
-    pub albedo: Vec3,
+	pub albedo: Vec3,
 }
 #[derive(Copy, Clone)]
 pub struct Metal {
-    pub albedo: Vec3,
+	pub albedo: Vec3,
+	pub fuzz: f64,
 }
 
 impl Lambertian {
-    // default material
-    pub fn new_from_vector(a: &Vec3) -> Self {
-        Lambertian { albedo: *a }
-    }
-    pub fn set(a: f64, b: f64, c: f64) -> Self {
-        Self {
-            albedo: Vec3::set(a, b, c),
-        }
-    }
+	// default material
+	pub fn new_from_vector(a: &Vec3) -> Self{
+		Lambertian {
+			albedo: *a,
+		}
+	}
+	pub fn set(a: f64, b: f64, c: f64) -> Self {
+		Self {
+			albedo: Vec3::set(a, b, c)
+		}
+	}
 }
 impl Metal {
-    pub fn new_from_vector(a: &Vec3) -> Self {
-        Metal { albedo: *a }
-    }
-    pub fn set(a: f64, b: f64, c: f64) -> Self {
-        Self {
-            albedo: Vec3::set(a, b, c),
-        }
-    }
+	pub fn new_from_vector(a: &Vec3, f: f64) -> Self{
+		Self {
+			albedo: *a,
+			fuzz: f,
+		}
+	}
+	pub fn set(a: f64, b: f64, c: f64, f: f64) -> Self {
+		Self {
+			albedo: Vec3::set(a, b, c),
+			fuzz: f,
+		}
+	}
 }
 
 impl Material for Lambertian {
-    fn scatter(
-        &self,
-        _r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Vec3,
-        scattered: &mut Ray,
-    ) -> bool {
-        let mut scatter_direction = vec3_add(&rec.normal, &random_unit_vector());
-        if scatter_direction.near_zero() {
-            scatter_direction.copy_vector(&rec.normal);
-        }
-        scattered.copy_ray(&Ray::set(rec.p, scatter_direction));
-        attenuation.copy_vector(&self.albedo);
-        true
-    }
+	fn scatter(&self, _r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
+		let mut scatter_direction = vec3_add(&rec.normal, &random_unit_vector());
+		if scatter_direction.near_zero() {
+			scatter_direction.copy_vector(&rec.normal);
+		}
+		scattered.copy_ray(&Ray::set(rec.p, scatter_direction));
+		attenuation.copy_vector(&self.albedo);
+		true
+	}
 }
 impl Material for Metal {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Vec3,
-        scattered: &mut Ray,
-    ) -> bool {
-        let reflected = reflect(&generate_unit_vector(&r_in.direction), &rec.normal);
-        scattered.copy_ray(&Ray::set(rec.p, reflected));
-        attenuation.copy_vector(&self.albedo);
-        vec3_dot(&scattered.direction, &rec.normal) > 0.0
-    }
+	fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
+		let reflected = reflect(&generate_unit_vector(&r_in.direction), &rec.normal);
+		scattered.copy_ray(&Ray::set(rec.p, vec3_add(&reflected, &vec3_mul(&self.fuzz, &random_in_unit_sphere()))));
+		attenuation.copy_vector(&self.albedo);
+		vec3_dot(&scattered.direction, &rec.normal) > 0.0
+	}
 }
 
 #[derive(Copy, Clone)]
 pub enum Materials {
-    MetalMaterials(Metal),
-    LambertianMaterials(Lambertian),
+	MetalMaterials(Metal),
+	LambertianMaterials(Lambertian),
 }
 
 impl Material for Materials {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Vec3,
-        scattered: &mut Ray,
-    ) -> bool {
-        // if let Materials::MetalMaterials(m) | Materials::LambertianMaterials(m) = self {
-        // 	m.scatter(r_in, rec, attenuation, scattered)
-        // } else {
-        // 	false
-        // }
-        match self {
-            Materials::LambertianMaterials(m) => m.scatter(r_in, rec, attenuation, scattered),
-            Materials::MetalMaterials(m) => m.scatter(r_in, rec, attenuation, scattered),
-        }
-    }
+	fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
+		match self {
+			Materials::LambertianMaterials(m) => {
+				m.scatter(r_in, rec, attenuation, scattered)
+			},
+			Materials::MetalMaterials (m) => {
+				m.scatter(r_in, rec, attenuation, scattered)
+			},
+		}
+	}
 }
 
 impl Default for Lambertian {
-    fn default() -> Self {
-        Lambertian {
-            albedo: Vec3::set(0.0, 0.0, 0.0),
-        }
-    }
+	fn default() -> Self {
+		Lambertian {
+			albedo: Vec3::set(0.0, 0.0, 0.0)
+		}
+	}
 }
 impl Default for Metal {
-    fn default() -> Self {
-        Metal {
-            albedo: Vec3::set(0.0, 0.0, 0.0),
-        }
-    }
+	fn default() -> Self {
+		Metal {
+			albedo: Vec3::set(0.0, 0.0, 0.0),
+			fuzz: 0.0,
+		}
+	}
 }
